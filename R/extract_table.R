2ac584400cabf1009bcc3a95dd85a5098eb6ebfc
Yifu Yan <yifyan@hotmail.com>
2019-10-11 16:16:42 -0700

    added utility function for getting coordinates from  google geocode api

---
+++ R/extract_table.R
@@ -0,0 +1,177 @@
+#' get rle as a named vector
+#'
+#' @param v a vector
+#' @return rle but with values as vector value and lengths as vector name
+vec_rle <- function(v){
+    temp <- rle(v)
+    out <- temp$values
+    names(out) <- temp$lengths
+    return(out)
+}
+
+
+#' returns the consecutive row/column numbers that are not NA in a list
+#' @description Only accept a boolean vector(only TRUE or FALSE). Otherwise, this
+#' function doesn't make any sense.
+#'
+#' @param v a boolean vector(only TRUE/FALSE)
+#'
+#' @return A list of consecutive row/column numbers as different elements
+make_df_index <- function(v){
+    # doesn't make sense if v is not a logical vector or contains NA
+    stopifnot(is.logical(v))
+    stopifnot(any(is.na(v)))
+    table_rle <- vec_rle(v)
+    divide_points <- c(0,cumsum(names(table_rle)))
+    table_index <- purrr::map2((divide_points + 1)[1:length(divide_points)-1],
+                        divide_points[2:length(divide_points)],
+                        ~.x:.y)
+    return(table_index[table_rle])
+}
+
+
+#' split a large table in one direction if there are blank columns or rows
+#'
+#' @param df a data frame
+#' @param direction "col" or "row", the direction to split the data frame
+#' @param split_index whether to return the index where the split is made
+#' @return
+#' If split_index is \code{TRUE}, returns a list of data frame.
+#' If split_index is \code{FALSE}, returns a list of two elements, the first element
+#' \code{df} contains a list of split data frames, the second element \code{split_index}
+#' is the index where the split is performed.
+#' @export
+du_split_direction <- function(df,direction = "col",split_index = FALSE){
+    if(direction == "col"){
+        direct_has_data <- unname(map_lgl(df,~!all(is.na(.x))))
+        df_mapping <- make_df_index(direct_has_data)
+        out <- map(df_mapping,~df[,.x])
+    } else if(direction == "row"){
+        direct_has_data <- df %>%
+            dplyr::mutate_all(~!is.na(.x)) %>%
+            purrr::pmap_lgl(any)
+        df_mapping <- make_df_index(direct_has_data)
+        out <- purrr::map(df_mapping,~df[.x,])
+    }
+
+    if (split_index) {
+        return(
+            list(
+                df = out,
+                split_index = setdiff(
+                    1:length(direct_has_data),
+                    purrr::reduce(df_mapping,c)
+                )
+            )
+        )
+    } else {
+        return(out)
+    }
+}
+
+
+
+#' split a large table into smaller tables if there are blank columns or rows
+#'
+#' @param df a data frame
+#' @param complexity default to 1,number of times to split a data frame by row and column.
+#' If number increase by one, one more split(by both row and column) will be performed
+#' @details
+#'
+#' 1. if you still see entire rows or columns missing. Please increase complexity
+#' 2. The list of data frames is roughly ordered from left to right and top to
+#' bottom in the original data frame
+#'
+#' @return a list of data frames
+#'
+#' @examples
+#' mydf <- as.data.frame(matrix(1:100,nrow = 10))
+#' mydf[5,] <- NA
+#' mydf[,7] <- NA
+#' mydf[,8 ] <- NA
+#' mydf[1,c(1,2,3)] <- NA
+#' du_split_df(mydf)
+#'
+#' @export
+du_split_df <- function(df,complexity = 1){
+    out <- list(df)
+    for(i in 1 :complexity){
+        for (direction in c("row","col")){
+            split_result <- out %>%
+                map(~du_split_direction(.x,direction))
+            out <- flatten(split_result)
+        }
+    }
+    return(out)
+}
+
+
+#' display cells in a data frame that doesn't contain missing value
+#' @param df a data frame
+#'
+#' @return a graph that display the shape of data
+#'
+#' @export
+du_display_data_shape <- function(df){
+    colnames(df) <- 1:ncol(df)
+    df %>%
+        map_df(~as.numeric(!is.na(.x))) %>%
+        gather(key = "x",value = "value") %>%
+        mutate(x = as.numeric(x)) %>%
+        group_by(x) %>%
+        mutate(y = -row_number()) %>%
+        ungroup() %>%
+        filter(value == 1) %>%
+        ggplot(aes(x = x, y = y,fill = value)) +
+        geom_tile(fill = "skyblue3") +
+        scale_x_continuous(position = "top") +
+        geom_rect(
+            aes(xmin = 0,xmax = dim(df)[1],ymin = -dim(df)[2],ymax = 0),
+            position = position_nudge(x = 0.5, y = -0.5),
+            alpha = 0,
+            color = "black",
+            size = 1
+        ) +
+        coord_fixed() +
+        theme_void() +
+        guides(fill = FALSE)
+}
+
+#' read multiple ranges from the same sheet in an excel file
+#'
+#' @param path file of excel file
+#' @param sheet the sheet name or number to be read. If NULL, read the first sheet
+#' @param range range(s) in the given sheet, can be a unnamed/named vector or list.
+#' If named, names will be used as the name in the list of data frames
+#' @param guess whether to guess column type, if FALSE, read all columns as text
+#'
+#' @return a list of data frames
+#'
+#' @examples
+#' \dontrun{
+#' file_path <- system.file("examples/excel.xlsx",package = "dfutils")
+#' range1 <- c("A1:E5","H7:L11")
+#' range2 <- list(tabl1="A1:E5",table2="H7:L11")
+#' du_read_excel_ranges(file_path,range = range1)
+#' du_read_excel_ranges(file_path,range = range2,guess = FALSE)
+#' }
+#'
+#' @export
+du_read_excel_ranges <- function(path,sheet = NULL,range,guess = TRUE){
+    #default to read everything as string
+    if(guess){
+        df_list <- purrr::map(range,function(ran){
+            readxl::read_excel(path,sheet,range = ran)
+        })
+    } else {
+        df_list <- purrr::map(range,function(ran){
+            readxl::read_excel(path,sheet,range = ran,col_types = "text")
+        })
+    }
+    if(!is.null(names(range))){
+        names(df_list) <- names(range)
+    }
+
+
+    return(df_list)
+}
---
+++ R/google_api.R
@@ -0,0 +1,258 @@
+#' get geocode safely, it will never be interrupted
+#'
+#' @param address address, vector length of one
+#' @param apikey google's apikey
+#'
+#' @return a list of google's geocode service result
+safely_get_geocode <- function(address,apikey){
+    safe_func <- purrr::safely(function(){
+        response <- googleway::google_geocode(address,key = apikey,simplify = TRUE)
+        coords <- response[["results"]][["geometry"]][["location"]]
+        return(
+            list(
+                response = response,
+                coords = coords
+            )
+        )
+    })
+    return(safe_func())
+}
+
+#' get geocode with error handling built in,reasons are given for failed items
+#'
+#' @description getting geocode can be tedious sometimes since sometimes we get
+#' errors, sometimes we multiple matched results. And doing the error/exception
+#' handling has always been a headache. This functions tries do the error handling
+#' for your api call won't be interrupted by any errors and you receive clean
+#' results.
+#'
+#' Note that since this function won't be interrupted, please always test in smaller
+#' scale to make sure everything is working as intended.
+#'
+#' @param address a character vector that contains address(es)
+#' @param apikey google's apikey for using geocode service
+#' @param result_type how should the results be returned, see more in details
+#' @param multi_coord_handling how to handle results with multiple matching coordinates
+#' @param progress whether to show a progress bar
+#' @param show_summary whether to show a summary on job status
+#'
+#' @return a list of two elements \code{result} and \code{error}, return details
+#' are dependent on \code{result_type} argument.
+#'
+#' @details
+#'
+#' \strong{result_type:}
+#'
+#' This function returns a list of two parts, \code{result} and \code{error}, and
+#' result_type determines how \code{result} is structured. If a item fails or has
+#' been processed by this function internally, a comment will show. Otherwise,
+#' comment will be NA.
+#'
+#' \code{result}
+#'
+#' \describe{
+#'     \item{list}{returns a list of 3 elements, lat,lng and comment}
+#'     \item{df}{returns a data frame with 3 columns, lat, lng and comment}
+#'     \item{raw}{returns the raw result from google's api as a list. Don't show
+#'     any comments and this function don't process any data
+#'     (\code{multi_coord_handling} won't be applied),you receive everything
+#'     provided by geocode api}
+#' }
+#'
+#' \code{error}
+#'
+#' a list that shows the error or special treatment for items in \code{result}
+#'
+#' \strong{multiple_coord_handling}
+#'
+#' Sometimes an address can be confusing to google's geocode API and multiple
+#' matches are returned. For data analysis, It is undesirable in most cases. So
+#' this functions tries handle this situation automatically. Regardless how this
+#' functions handles this scenario, one entry is generated in \code{error}.
+#'
+#' \describe{
+#'     \item{none}{no hanlding, the coordinates will be NA}
+#'     \item{first}{use the first coordinates provided}
+#'     \item{average}{average all coordinates and use it as result}
+#' }
+#'
+#' \strong{show_summary}
+#'
+#' summary will be printed to console, it shows how many addresses are tried in
+#' total, how many succeeded or failed
+#'
+#' \strong{comment}
+#'
+#' Below are explanations for comments:
+#'
+#' \itemize{
+#'     \item API Error: something's wrong during API call
+#'     \item Multiple Matches Error: Multiple paries of coordinates found for the
+#'     same address.
+#'     \item Multiple Matches Found, Using First Result: use the first result returned
+#'     by geocode API
+#'     \item Multiple Matches Found, Using Average Coordinates: use the average of
+#'     coordinates returned by geocode API
+#' }
+#'
+#' @examples
+#' \dontrun{
+#' apikey = your_apikey
+#' du_google_geocode(
+#'     "new york, usa",
+#'     "#####", # contains error
+#'     "canada, usa" # multiple matching results
+#' )
+#' }
+#' @export
+du_google_geocode <- function(address,
+                              apikey,
+                              result_type = c("list","df","raw"),
+                              multi_coord_handling = c("none","first","average"),
+                              progress = TRUE,
+                              show_summary = TRUE){
+    # check whether package is loaded
+    if(!("googleway" %in% (.packages()))){
+        stop("du_google_geocode: This function must run with googleway packge loaded")
+    }
+    # whether to show progress
+    if (progress) {
+        pb <- dplyr::progress_estimated(length(address))
+        safe_results <- purrr::map(address,function(address){
+            pb$tick()$print()
+            safely_get_geocode(address,apikey)
+        })
+    } else {
+        safe_results <- purrr::map(address,function(address){
+            safely_get_geocode(address,apikey)
+        })
+    }
+    # first element will be applied
+    result_type <- result_type[1]
+    multi_coord_handling <- multi_coord_handling[1]
+    # validate user input
+    stopifnot(result_type %in% c("list","df","raw"))
+    stopifnot(multi_coord_handling %in% c("none","first","average"))
+
+    # error messages
+    request_errors <- purrr::keep(
+        setNames(
+            purrr::map(safe_results, "error"),
+            1:length(address)
+        ),
+        ~!is.null(.x)
+    )
+    # early return
+    if(result_type == "raw"){
+        return(
+            list(
+                result = setNames(
+                    map(safe_results,~.x[["result"]][["response"]]),
+                    1:length(address)
+                ),
+                error = request_errors
+            )
+        )
+    } else {
+        safe_df_list <- purrr::map(safe_results,~.x[["result"]][["coords"]])
+        handling_info <- list()
+        result_returned <- list()
+        for (i in 1:length(safe_df_list)) {
+            ele = safe_df_list[[i]]
+            response_i = safe_results[[i]][["result"]][["response"]]
+            # returns NA for missing values
+            if(is.null(ele)){
+                result_returned <- append(
+                    result_returned,
+                    setNames(
+                        list(
+                            data.frame(
+                                lat = NA,
+                                lng = NA,
+                                comment = "API Error.",
+                                stringsAsFactors = FALSE
+                            )
+                        ),
+                        i
+                    )
+                )
+                # no processing for df with one row
+            } else if (is.data.frame(ele) & nrow(ele) == 1) {
+                ele["comment"] = NA
+                ele_m <- ele
+                result_returned <- append(
+                    result_returned,
+                    setNames(list(ele_m),i)
+                )
+            } else if (is.data.frame(ele) & nrow(ele) > 1) {
+                handling_info <-append(
+                    handling_info,
+                    setNames(list(response_i),i)
+                )
+                if (multi_coord_handling == "none") {
+                    result_returned <- append(
+                        result_returned,
+                        setNames(
+                            list(
+                                data.frame(
+                                    lat = NA,
+                                    lng = NA,
+                                    comment = "Multiple Matches Error.",
+                                    stringsAsFactors = FALSE
+                                )
+                            ),
+                            i
+                        )
+                    )
+                } else if (multi_coord_handling == "first") {
+                    ele["comment"] = "Multiple Matches Found, Using First Result"
+                    ele_m <- ele[1,]
+                    result_returned <- append(
+                        result_returned,
+                        setNames(list(ele_m),i)
+                    )
+                } else if (multi_coord_handling == "average") {
+                    ele_m <- dplyr::summarise_all(ele,mean)
+                    ele_m["comment"] = "Multiple Matches Found, Using Average Coordinates"
+                    result_returned <- append(
+                        result_returned,
+                        setNames(list(ele_m),i)
+                    )
+                }
+            }
+        }
+        error_element <- append(request_errors,handling_info)
+        if(length(error_element) > 0) {
+            error_element <- error_element[order(names(error_element))]
+        }
+        result_df <- bind_rows(result_returned)
+
+        if(show_summary){
+
+            cat(
+                sprintf("\nTotal addresses: %s, Failed: %s",
+                        length(address), length(error_element))
+            )
+        }
+        if (result_type == "list") {
+            return(
+                list(
+                    result = setNames(
+                        purrr::map(result_returned,~as.list(.x)),
+                        1:length(address)
+                    ),
+                    error = error_element
+                )
+            )
+        }
+
+        if (result_type == "df"){
+            return(
+                list(
+                    result = result_df,
+                    error = error_element
+                )
+            )
+        }
+    }
+}
